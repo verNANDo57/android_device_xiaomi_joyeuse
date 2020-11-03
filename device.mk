06e761df321df5c71d939b5d40c83937d43f91a7
verNANDo57 <silvenation@gmail.com>
2020-11-04 00:16:25 +0700

    joyeuse: move libstdc++.vendor to common tree [2/2]

--- device.mk
+++ device.mk
@@ -67,10 +67,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_joyeuse/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_joyeuse/com.nxp.mifare.xml
 
-# Camera
-PRODUCT_PACKAGES += \
-    libstdc++.vendor
-
 # Secure element
 PRODUCT_PACKAGES += \
     SecureElement
