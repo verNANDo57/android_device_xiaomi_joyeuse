8e6778d353287f29ff89f3eb30f72895a59f8b95
verNANDo57 <silvenation@gmail.com>
2020-11-03 13:45:28 +0700

    joyeuse: import libstdc++.vendor

--- device.mk
+++ device.mk
@@ -67,6 +67,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_joyeuse/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_joyeuse/com.nxp.mifare.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    libstdc++.vendor
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
