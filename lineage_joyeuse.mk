544a67eab4530311a1c7c69f5a68e87a1be8b158
verNANDo57 <silvenation@gmail.com>
2020-11-06 23:47:00 +0700

    sm6250-common: set IS_PHONE value to true

--- lineage_joyeuse.mk
+++ lineage_joyeuse.mk
@@ -3,11 +3,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 $(call inherit-product, device/xiaomi/joyeuse/device.mk)
 
 # Inherit some common Lineage OS stuff
@@ -25,3 +20,5 @@ PRODUCT_MODEL := Redmi Note 9 Pro
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR_PRODUCT_NAME := joyeuse
+
+IS_PHONE := true
