878acbe80e33637403410a4e9e7f1c1126c99a2c
verNANDo57 <silvenation@gmail.com>
2020-11-07 23:05:05 +0700

    joyeuse: revert commit e73675ef3863934a434f16e56e6a159e199af8c7

--- lineage_joyeuse.mk
+++ lineage_joyeuse.mk
@@ -25,8 +25,3 @@ PRODUCT_MODEL := Redmi Note 9 Pro
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR_PRODUCT_NAME := joyeuse
-
-BUILD_FINGERPRINT :="google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys" 
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="joyeuse-user 10 QKQ1.191215.002/V12.0.1.0.QJZMIXM release-keys"
