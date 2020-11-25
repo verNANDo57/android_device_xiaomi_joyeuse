2bbb32965b9f832b453f9eb5d0f9d1f17051d97b
verNANDo57 <silvenation@gmail.com>
2020-11-25 19:23:14 +0700

    joyeuse: import audio_io_policy.conf

--- device.mk
+++ device.mk
@@ -93,7 +93,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_qrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths_qrd.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd937x.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths_wcd937x.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml \
-    $(LOCAL_PATH)/audio/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+    $(LOCAL_PATH)/audio/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf
 
 # Media
 PRODUCT_COPY_FILES += \
--- props.mk
+++ props.mk
@@ -9,13 +9,13 @@ ro.debuggable=0
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-vendor.camera.aux.packageblacklist=com.discord \
-persist.camera.HAL3.enabled=1 \
-vendor.camera.aux.packagelist=org.lineageos.snap,com.android.camera,com.google.android.GoogleCamera \
-vendor.camera.aux.packagelist2=com.google.android.GoogleCameraWide,com.dual.GCam,com.Wide.GCam,com.Tele.GCam \
-vendor.camera.hal1.packagelist=com.whatsapp \
-persist.camera.dual.camera=0 \
-persist.camera.eis.enable=1
+     vendor.camera.aux.packageblacklist=com.discord \
+     persist.camera.HAL3.enabled=1 \
+     vendor.camera.aux.packagelist=org.lineageos.snap,com.android.camera,com.google.android.GoogleCamera \
+     vendor.camera.aux.packagelist2=com.google.android.GoogleCameraWide,com.dual.GCam,com.Wide.GCam,com.Tele.GCam \
+     vendor.camera.hal1.packagelist=com.whatsapp \
+     persist.camera.dual.camera=0 \
+     persist.camera.eis.enable=1
 
 #Recorder
 PRODUCT_PROPERTY_OVERRIDES += \
