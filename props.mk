90d46b9c377de733a94cb23ebf14088bd8659fd6
verNANDo57 <silvenation@gmail.com>
2020-12-29 13:09:15 +0700

    joyeuse: override all fingerprint blobs (odm, system, bootimage, vendor, product)

--- props.mk
+++ props.mk
@@ -26,3 +26,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.gralloc.disable_ubwc=0 \
     ro.hardware.egl=adreno \
     ro.hardware.vulkan=adreno
+
+# Fingerprint (Spoof)
+PRODUCT_PROPERTY_OVERRIDES += \
+   ro.product.build.fingerprint=Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.1.0.QJZMIXM:user/release-keys
+
+VENDOR_PROPERTY_OVERRIDES += \
+   ro.vendor.build.fingerprint=Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.1.0.QJZMIXM:user/release-keys \
+   ro.bootimage.build.fingerprint=Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.1.0.QJZMIXM:user/release-keys
+
+ODM_PROPERTY_OVERRIDES += \
+   ro.odm.build.fingerprint=Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.1.0.QJZMIXM:user/release-keys
+
+SYSTEM_PROPERTY_OVERRIDES += \
+   ro.system.build.fingerprint=Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.1.0.QJZMIXM:user/release-keys
+
+BUILD_FINGERPRINT := Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.1.0.QJZMIXM:user/release-keys
