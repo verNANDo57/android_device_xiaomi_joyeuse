38c4762783813023c7c877f746dba0f7c225eb2a
verNANDo57 <silvenation@gmail.com>
2020-12-31 13:50:15 +0700

    joyeuse: Switch to Redfin's Dec 2020 fingerprint

--- fingerprint.mk
+++ fingerprint.mk
@@ -1,23 +1,23 @@
 # Fingerprint (Spoof)
 PRODUCT_PROPERTY_OVERRIDES += \
-   ro.product.build.fingerprint=Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.1.0.QJZMIXM:user/release-keys
+   ro.product.build.fingerprint=google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys
 
 VENDOR_PROPERTY_OVERRIDES += \
-   ro.vendor.build.fingerprint=Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.1.0.QJZMIXM:user/release-keys \
-   ro.bootimage.build.fingerprint=Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.1.0.QJZMIXM:user/release-keys
+   ro.vendor.build.fingerprint=google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys \
+   ro.bootimage.build.fingerprint=google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys \
+   ro.product.build.fingerprint=google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys
 
 ODM_PROPERTY_OVERRIDES += \
-   ro.odm.build.fingerprint=Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.1.0.QJZMIXM:user/release-keys
+   ro.odm.build.fingerprint=google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys
 
 SYSTEM_PROPERTY_OVERRIDES += \
-   ro.system.build.fingerprint=Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.1.0.QJZMIXM:user/release-keys \
-   ro.build.fingerprint=Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.1.0.QJZMIXM:user/release-keys
+   ro.system.build.fingerprint=google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys \
+   ro.build.fingerprint=google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys
 
-BUILD_FINGERPRINT := Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.1.0.QJZMIXM:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys
 
-# Match security patch prop values to make fingerprint working
+# BUILD_FINGERPRINT := Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.1.0.QJZMIXM:user/release-keys
+
+# Description
 SYSTEM_PROPERTY_OVERRIDES += \
-   ro.build.version.security_patch=2020-10-05
-
-VENDOR_PROPERTY_OVERRIDES += \
-   ro.vendor.build.security_patch=2020-10-05
+   ro.build.description=joyeuse_global-user 10 QKQ1.191215.002 V12.0.1.0.QJZMIXM release-keys
--- init/init.cpp
+++ init/init.cpp
@@ -74,7 +74,7 @@ void set_device_props(const std::string fingerprint, const std::string descripti
 
 void load_device_properties() {
         set_device_props(
-                "Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.1.0.QJZMIXM:user/release-keys",
+                "google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys",
                 "joyeuse_global-user 10 QKQ1.191215.002 V12.0.1.0.QJZMIXM release-keys",
                 "Redmi", "joyeuse", "Redmi Note 9 Pro");
 }
