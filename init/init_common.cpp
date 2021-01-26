775d61361c7f91cbda884ffb97d1eddb573eb813
verNANDo57 <silvenation@gmail.com>
2021-01-26 18:08:15 +0700

    joyeuse: move init-on-boot script to android_device_joyeuse repo

---
+++ init/init_common.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cstring>
+#include <sys/sysinfo.h>
+
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+#include "init_common.h"
+#include "property_service.h"
+
+using android::init::property_set;
+
+void load_dalvik_properties() {
+    char const *heapstartsize;
+    char const *heapgrowthlimit;
+    char const *heapsize;
+    char const *heapminfree;
+    char const *heapmaxfree;
+    char const *heaptargetutilization;
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+
+    // from - phone-xhdpi-4096-dalvik-heap.mk
+    heapstartsize = "8m";
+    heapgrowthlimit = "192m";
+    heapsize = "512m";
+    heaptargetutilization = "0.75";
+    heapminfree = "512k";
+    heapmaxfree = "8m";
+
+    property_set("dalvik.vm.heapstartsize", heapstartsize);
+    property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_set("dalvik.vm.heapsize", heapsize);
+    property_set("dalvik.vm.heaptargetutilization", heaptargetutilization);
+    property_set("dalvik.vm.heapminfree", heapminfree);
+    property_set("dalvik.vm.heapmaxfree", heapmaxfree);
+}
+
+void load_common_properties() {
+    load_dalvik_properties();
+}
