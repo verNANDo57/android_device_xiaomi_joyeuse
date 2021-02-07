4e9286cd2c6121a1e8cd7b79e036d6b2568adfc5
verNANDo57 <silvenation@gmail.com>
2021-02-07 22:50:24 +0700

    joyeuse: switch to Feb 2021 redfin fingerprint

--- fingerprint.mk
+++ fingerprint.mk
@@ -1,18 +1,18 @@
 # Fingerprint (Spoof)
 PRODUCT_PROPERTY_OVERRIDES += \
-   ro.product.build.fingerprint=google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
+   ro.product.build.fingerprint=google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys
 
 VENDOR_PROPERTY_OVERRIDES += \
-   ro.vendor.build.fingerprint=google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys \
-   ro.bootimage.build.fingerprint=google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
+   ro.vendor.build.fingerprint=google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys \
+   ro.bootimage.build.fingerprint=google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys
 
 ODM_PROPERTY_OVERRIDES += \
-   ro.odm.build.fingerprint=google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
+   ro.odm.build.fingerprint=google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys
 
 SYSTEM_PROPERTY_OVERRIDES += \
-   ro.system.build.fingerprint=google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
+   ro.system.build.fingerprint=google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys
 
 # BUILD_FINGERPRINT := Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.2.0.QJZMIXM:user/release-keys
 
