16b0bdb7b9986d50ae57e92539208b08e6e6a42c
verNANDo57 <silvenation@gmail.com>
2021-04-25 14:44:59 +0700

    joyeuse: update fingerprint

--- fingerprint.mk
+++ fingerprint.mk
@@ -1,8 +1,8 @@
 # Fingerprint (Spoof)
 BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
 
-# BUILD_FINGERPRINT := Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.2.0.QJZMIXM:user/release-keys
+# BUILD_FINGERPRINT := Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V12.0.3.0.QJZMIXM:user/release-keys/1619079719
 
 # Description
 SYSTEM_PROPERTY_OVERRIDES += \
-   ro.build.description=joyeuse_global-user 10 QKQ1.191215.002 V12.0.2.0.QJZMIXM release-keys
+   ro.build.description=joyeuse_global-user 10 QKQ1.191215.002 V12.0.3.0.QJZMIXM release-keys
