580179954278feead72432dfbebc4231edfa22eb
verNANDo57 <silvenation@gmail.com>
2021-05-07 14:37:13 +0700

    joyeuse: megvii_shim: update shim && add missing methods

--- camera/megvii_shim.cpp
+++ camera/megvii_shim.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 The LineageOS Project
+ * Copyright (C) 2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,4 +16,10 @@
 
 namespace android {
     extern "C" void mg_facepp() {}
+
+    extern "C" void _ZN12megvii_cloud6facepp11FPP_GetRectEP11_MG_FPP_APIiiP14MG_DETECT_RECT() {}
+
+    extern "C" void _ZN12megvii_cloud6facepp15FPP_GetLandmarkEP11_MG_FPP_APIiiiP8MG_POINT() {}
+
+    extern "C" void _ZN12megvii_cloud6facepp19FPP_GetFaceLandmarkEP11_MG_FPP_APIiiiP8MG_POINT() {}
 }
