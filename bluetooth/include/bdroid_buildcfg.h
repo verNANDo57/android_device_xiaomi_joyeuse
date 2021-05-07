f1acbc0e86235535ceadff0b12024abad99a10eb
verNANDo57 <silvenation@gmail.com>
2021-05-07 14:31:11 +0700

    joyeuse: bluetootjh_config: setup Wide-band speech support

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -26,8 +26,15 @@
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    32
-#define BLE_VND_INCLUDED   TRUE
+
+// Vendor extensions
+#define BLE_VND_INCLUDED TRUE
+
 #define GATT_MAX_PHY_CHANNEL  10
+
+// Wide-band speech support
+#define BTM_WBS_INCLUDED TRUE
+#define BTIF_HF_WBS_PREFERRED TRUE
 
 // QCOM power management workaround
 #define BT_CLEAN_TURN_ON_DISABLED TRUE
