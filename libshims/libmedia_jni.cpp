64d873bfe04af5b8506d9f8064b57678b9f48f31
verNANDo57 <silvenation@gmail.com>
2021-05-28 00:44:00 +0700

    joyeuse: shim: get rid of useless empty constructor

--- libshims/libmedia_jni.cpp
+++ libshims/libmedia_jni.cpp
@@ -5,11 +5,7 @@ const std::string mOpPackageName = "";
 // New constructor prototype of MediaPlayer::MediaPlayer(const std::string opPackageName)
 extern "C" void _ZN7android11MediaPlayerC1ENSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE(const std::string opPackageName);
 
-// Old constructors
+// Old constructor
 extern "C" void _ZN7android11MediaPlayerC1Ev() {
     return _ZN7android11MediaPlayerC1ENSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE(mOpPackageName);
-}
-
-extern "C" void _ZNSt3__112basic_stringIcNS_11char_traitsIcEENS_9allocatorIcEEEC1ERKS5_ ()  {
-		// Required empty constructor
 }
