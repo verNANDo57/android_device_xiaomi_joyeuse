ede98d5f018b60b6f5d65e9627ab063315f3bed8
verNANDo57 <silvenation@gmail.com>
2021-06-08 13:06:08 +0700

    joyeuse: props.mk: set ro.adb.secure -> 1 (fixes "no permissions" issue while using ADB)

--- props.mk
+++ props.mk
@@ -6,6 +6,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # USB debugging
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp,adb \
-    ro.adb.secure=0 \
+    ro.adb.secure=1 \
     ro.secure=0 \
     ro.debuggable=1
