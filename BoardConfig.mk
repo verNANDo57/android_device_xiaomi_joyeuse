e312cba60a28254d3393492b8a08e463f53374b7
verNANDo57 <silvenation@gmail.com>
2021-06-08 12:49:12 +0700

    joyeuse: sepolicy: combine BoardConfig.mk and joyeuse-sepolicy.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -287,7 +287,19 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD := true
 
 # Sepolicy
-include device/xiaomi/joyeuse/sepolicy/joyeuse-sepolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
+
+# BOARD_PLAT_* DIRS ONLY KEPT FOR COMPATIBILITY REASONS. WILL BE A HARD ERROR ON API LEVEL 31
+# SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+#     device/xiaomi/joyeuse/sepolicy/private
+# SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+#     device/xiaomi/joyeuse/sepolicy/public
+# BOARD_VENDOR_SEPOLICY_DIRS += \
+#     device/xiaomi/joyeuse/sepolicy/vendor
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
