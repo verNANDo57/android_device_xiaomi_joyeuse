484fdc622f22a4c6aab2b00b8524716041cd91dc
verNANDo57 <silvenation@gmail.com>
2021-08-04 18:38:06 +0700

    joyeuse: introduce dummy libqti-perfd-client
    - proprietary perfd blobs can finally be nuked without breaking goodix
    - we could even map the functions to use libperfmgr powerhints in the future

---
+++ libqti-perfd-client/client.cpp
@@ -0,0 +1,25 @@
+#include <stdint.h>
+
+extern "C" void perf_get_feedback() {
+    // Required empty constructor
+}
+
+extern "C" void perf_hint() {
+    // Required empty constructor
+}
+
+extern "C" void perf_lock_acq() {
+    // Required empty constructor
+}
+
+extern "C" void perf_lock_cmd() {
+    // Required empty constructor
+}
+
+extern "C" void perf_lock_rel() {
+    // Required empty constructor
+}
+
+extern "C" void perf_lock_use_profile() {
+    // Required empty constructor
+}
