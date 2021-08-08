6c091854db58da5a58e505a95776ff4a18641a30
VerNANDo57 <49901327+verNANDo57@users.noreply.github.com>
2021-08-08 17:37:48 +0700

    joyeuse: switch back from proton clang to default clang
    We have to do this due to unexpected errors and issues which cannot be fixed for now. This is a temporary solution to this problem. Soon I will try to come up with an alternative way to solve this problem.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -188,11 +188,10 @@ TARGET_KERNEL_ARCH := arm64
 #TARGET_NEW_GCC_COMPILE := true
 
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := proton
+# TARGET_KERNEL_CLANG_VERSION := r383902b1
 TARGET_KERNEL_CONFIG := cust_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6250
-KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+# KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-r383902b1/bin
 KERNEL_SUPPORTS_LLVM_TOOLS := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
