a5d5647166e40fabe0c08477acbca783cc8ced96
verNANDo57 <silvenation@gmail.com>
2021-08-22 16:19:25 +0700

    joyeuse: bluetooth_config: change device name

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -22,7 +22,7 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME "Redmi Note 9 Series"
+#define BTM_DEF_LOCAL_NAME "Redmi Note 9 Pro"
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    32
