1c3d8d9019a880d0bfb1b467d52a5986ca2a3d91
verNANDo57 <silvenation@gmail.com>
2022-06-26 00:18:51 +0700

    joyeuse: device: build android.hardware.drm@1.4

--- device.mk
+++ device.mk
@@ -335,7 +335,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.3.vendor
 
 # Fastbootd
 PRODUCT_PACKAGES += \
