2fe66002ad9f2c48b0003038966ba896374a0e16
verNANDo57 <silvenation@gmail.com>
2022-06-25 22:48:47 +0700

    joyeuse: light: sm6250 -> joyeuse

--- device.mk
+++ device.mk
@@ -433,7 +433,7 @@ PRODUCT_COPY_FILES += \
 
 # Light
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_sm6250
+    android.hardware.light@2.0-service.xiaomi_joyeuse
 
 # ANT+
 PRODUCT_PACKAGES += \
