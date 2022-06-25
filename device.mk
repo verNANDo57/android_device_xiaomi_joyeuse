bf279a1e59d6e7ff522f027478988e8323e4d9d7
verNANDo57 <silvenation@gmail.com>
2022-06-25 22:46:36 +0700

    joyeuse: fingerprint: sm6250 -> joyeuse

--- device.mk
+++ device.mk
@@ -327,7 +327,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm6250
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_joyeuse
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
