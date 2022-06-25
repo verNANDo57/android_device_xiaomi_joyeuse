ef52645953359a2eaf407af4d3277be499870cd5
verNANDo57 <silvenation@gmail.com>
2022-06-26 00:56:13 +0700

    joyeuse: BoardConfig: BOARD_VNDK_VERSION -> current

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -211,6 +211,9 @@ SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SELINUX_IGNORE_NEVERALLOWS := true
 
+# Treble
+BOARD_VNDK_VERSION := current
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
