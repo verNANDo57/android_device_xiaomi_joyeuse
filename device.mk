9480fd3548cf4519a2e1b3e6942712953a6b2908
verNANDo57 <silvenation@gmail.com>
2022-06-29 22:26:23 +0700

    joyeuse: build android.hardware.gnss@1.1.vendor

--- device.mk
+++ device.mk
@@ -377,6 +377,7 @@ PRODUCT_PACKAGES += \
 
 # GNSS
 PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.1.vendor \
     android.hardware.gnss@2.1.vendor \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss.visibility_control@1.0.vendor
