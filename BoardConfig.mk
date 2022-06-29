0af29fd915c06c0b114f9c7e1fa35b574b83f5e9
verNANDo57 <silvenation@gmail.com>
2022-06-29 22:37:25 +0700

    joyeuse: BoardConfig: remove TARGET_KERNEL_CLANG_COMPILE
    * Since Android S TARGET_KERNEL_CLANG_COMPILE is always true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,7 +99,6 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := r437112b
 TARGET_KERNEL_CONFIG := cust_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6250
-TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
